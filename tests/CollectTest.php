6cc258cf39498d62c1af226f941cfe9c272619ef
AnastasiaKilko <anastasia.v.kilko@gmail.com>
2025-05-21 03:13:05 +0700

    unittests

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,13 +1,30 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-
+use Collect\Collect;
 class CollectTest extends TestCase
 {
+    public function testOnly()
+    {
+        $collect = new Collect(['id' => 2135, 'name' => 'Nastya', 'city' => 'Tomsk']);
+        $only = $collect->only('id', 'city');
+        $expected = ['id' => 2135, 'city' => 'Tomsk'];
+        $this->assertSame($expected, $only->toArray());
+    }
     public function testCount()
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        $collect = new Collect\Collect(['id' => 2135, 'name' => 'Nastya', 'city' => 'Tomsk']);
+        $this->assertSame(3, $collect->count());
     }
-
+    public function testFirst()
+    {
+        $collect = new Collect(['id' => 2135, 'name' => 'Nastya', 'city' => 'Tomsk']);
+        $this->assertSame(2135, $collect->first());
+    }
+    public function testToArray()
+    {
+        $data = ['id' => 2135, 'name' => 'Nastya', 'city' => 'Tomsk'];
+        $collect = new Collect($data);
+        $this->assertSame($data, $collect->toArray());
+    }
 }
